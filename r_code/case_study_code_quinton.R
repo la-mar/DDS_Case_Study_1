e162e1a212b0e86bff37d70c29b8283ea762933f
qatsmu <qnixon@mail.smu.edu>
2018-06-21 10:22:07 -0700

    now with trial hypotheses
    At the bottom of this script, I added a section attempted to come up
    with a testable, relevant hypothesis.  I think I found a good way to
    compare beer popularity according to abv or ibu.

--- r_code/case_study_code_quinton.R
+++ r_code/case_study_code_quinton.R
@@ -5,8 +5,8 @@ library(tidyverse)
 
 
 # read in data
-beers <- read.csv("data/Beers.csv", stringsAsFactors = FALSE)
-breweries <- read.csv("data/Breweries.csv", stringsAsFactors = FALSE)
+beers <- read.csv("Beers.csv", stringsAsFactors = FALSE)
+breweries <- read.csv("Breweries.csv", stringsAsFactors = FALSE)
 
 # calculate breweries by state
 breweries_by_state <- breweries %>% count(State) %>% arrange(desc(n))
@@ -15,7 +15,7 @@ colnames(breweries_by_state) <- c("state", "brewery_count")
 # merge beers and breweries
 beers_by_brewery <- merge(beers, breweries, by.x = "Brewery_id", by.y = "Brew_ID")
 colnames(beers_by_brewery) <- c("brewery_id", "beer_name", "beer_id", "abv",
-                                "ibu", "stye", "ounces", "brewery_name", 
+                                "ibu", "style", "ounces", "brewery_name", 
                                 "brewery_city", "brewery_state")
 head(beers_by_brewery, 6)
 tail(beers_by_brewery, 6)
@@ -60,3 +60,36 @@ abv_summary <- summary(beers_by_brewery$abv)
 # bitterness vs. abv plot
 plot(beers_by_brewery$ibu, beers_by_brewery$abv)
 
+
+# my take on coming up with a way to make a null hypothesis or two
+# I calculated the number of states in which a style of beer is brewed and
+# took the median.  Beer styles brewed in more states than the median were
+# assigned "high" popularity and styles brewed in fewer states than the median 
+# were assigned "low" popularity.  Then I ran t-tests for abv and ibu.
+# using this method, there is a difference in the average abv for high popularity
+# beers vs. low.  There wasn't a difference in bitterness.  So our case study
+# could be Buzz vs. Bite:  Does alcohol content or taste drive beer popularity
+
+number_of_states_brewing_a_beer_type <- beers_by_brewery %>% 
+  group_by(style) %>% summarise(num_of_states = n_distinct(brewery_state))
+
+median_states <- median(number_of_states_brewing_a_beer_type$num_of_states)
+
+for (i in 1:nrow(number_of_states_brewing_a_beer_type)){
+  if (number_of_states_brewing_a_beer_type$num_of_states[i] < median_states) {
+    number_of_states_brewing_a_beer_type$popularity[i] <- "low"
+  } else {
+    number_of_states_brewing_a_beer_type$popularity[i] <- "high"
+  }
+} 
+
+beers_by_brewery <- merge(beers_by_brewery, number_of_states_brewing_a_beer_type,
+                          by.x = "style", by.y = "style")
+
+seeking_h_null_abv <- t.test(beers_by_brewery$abv ~ beers_by_brewery$popularity,
+                             na.action=na.omit)
+seeking_h_null_ibu <- t.test(beers_by_brewery$ibu ~ beers_by_brewery$popularity,
+                             na.action=na.omit)
+
+
+write.csv(beers_by_brewery, "beers_by_brewery.csv", row.names = FALSE)
